7cd25cc499d72bd823344d113b61349bcd3e80da
justinwmarchant <marchantjustinw@gmail.com>
2023-01-26 18:26:15 -0700

    fixing crop to only allow 1x1 aspect ratio and other small changes

--- frontend/pages/UpLoad.jsx
+++ frontend/pages/UpLoad.jsx
@@ -17,7 +17,7 @@ function UpLoad() {
   };
 
   const setCroppedImageFor = (id, crop, zoom, aspect, croppedImageUrl) => {
-    const newImage = { ...image, croppedImageUrl, crop, zoom, aspect};
+    const newImage = { ...image, croppedImageUrl, crop, zoom, aspect };
     setImage(newImage);
     setSelectedImage(null);
   };
@@ -26,31 +26,37 @@ function UpLoad() {
     setCroppedImageFor(id);
   };
 
+  const onImageUpload = (event) => {
+    const uploadedImage = {...image}
+    uploadedImage.imageUrl = URL.createObjectURL(event.target.files[0])
+    setImage(uploadedImage)
+  }
+
   return (
     <div className='Upload'>
-      {selectedImage ? (
-        <ImageCrop
-          id={selectedImage.id}
-          imageUrl={selectedImage.imageUrl}
-          cropInit={selectedImage.crop}
-          zoomInit={selectedImage.zoom}
-          aspectInit={selectedImage.aspect}
-          onCancel={onCancel}
-          setCroppedImageFor={setCroppedImageFor}
-          resetImage={resetImage}
-        />
-      ) : null}
-      {/* <BG className='background' /> */}
-      {/* Displays the current step */}
-      {/* <div>upload</div> */}
-      {/* <input type='file' name='myImage' /> */}
-      <div className='imageCard'>
-        <img
-          src={image.croppedImageUrl ? image.croppedImageUrl : image.imageUrl}
-          alt=''
-          onClick={() => setSelectedImage(image)}
-        />
-      </div>
+      <BG className='background' />
+        {selectedImage ? (
+          <ImageCrop
+            id={selectedImage.id}
+            imageUrl={selectedImage.imageUrl}
+            cropInit={selectedImage.crop}
+            zoomInit={selectedImage.zoom}
+            aspectInit={selectedImage.aspect}
+            onCancel={onCancel}
+            setCroppedImageFor={setCroppedImageFor}
+            resetImage={resetImage}
+          />
+        ) : null}
+        <div>upload</div>
+        <input type='file' accept='image/*' name='myImage' onChange={onImageUpload}/> 
+
+        <div className='imageCard'>
+          <img
+            src={image.croppedImageUrl ? image.croppedImageUrl : image.imageUrl}
+            alt=''
+            onClick={() => setSelectedImage(image)}
+          />
+        </div>
     </div>
   );
 }
